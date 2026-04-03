38e9aa7d6e2795856394f5f7463208580de27e98
Mateo-Aguirre-04 <moppetmateo15@gmail.com>
2026-04-03 12:07:17 -0500

    Dash code

--- dash_gui.py
+++ dash_gui.py
@@ -1,10 +1,8 @@
-import datetime
-
 import pandas as pd
 from dash import Dash, dcc, html
 import plotly.express as px
 
-# Load cleaned dataset (from previous task)
+# Load cleaned dataset (from previous ta2sk)
 df = pd.read_csv("cleaned_data.csv")
 
 # Ensure proper datetime format and sorting
@@ -13,6 +11,16 @@ df = df.sort_values("date")
 
 # Aggregate sales by date
 daily_sales = df.groupby("date")["sales"].sum().reset_index()
+before = daily_sales[daily_sales["date"] < "2021-01-15"]["sales"].mean()
+after = daily_sales[daily_sales["date"] >= "2021-01-15"]["sales"].mean()
+
+print("Promedio antes:", before)
+print("Promedio después:", after)
+
+if after > before:
+    print("Las ventas fueron MAYORES después del aumento")
+else:
+    print("Las ventas fueron MENORES después del aumento")
 
 # Create line chart
 fig = px.line(
@@ -31,7 +39,7 @@ fig.add_vline(
 
 fig.add_annotation(
     x="2021-01-15",
-    y=daily_sales["total_sales"].max(),
+    y=daily_sales["sales"].max(),
     text="Price Increase",
     showarrow=True,
     arrowhead=2,
@@ -42,8 +50,16 @@ fig.add_annotation(
 app = Dash(__name__)
 
 # Layout
+result_text = (
+    "Sales increased after the price change"
+    if after > before
+    else "Sales decreased after the price change"
+)
+
 app.layout = html.Div([
     html.H1("Soul Foods Sales Visualizer", style={"textAlign": "center"}),
+
+    html.H3(result_text, style={"textAlign": "center"}),
 
     dcc.Graph(
         id="sales-line-chart",
